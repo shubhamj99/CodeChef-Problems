dfe67a30e10db7ffa3c6a31cd57621c1fac5e23a
Shubham Jain <43003103+shubhamj99@users.noreply.github.com>
2021-01-28 05:43:40 +0530

    Create  LuckyFour.cpp

---
+++ Beginner/LuckyFour.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int n;
+	    cin>>n;
+	    int count = 0;
+	    while(n>0){
+	        if(n%10 == 4){
+	            count++;
+	        }
+	        n /= 10;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
